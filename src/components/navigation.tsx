7bd6345f89e37973ee156d23e0e7a9d08b202d1c
Saga-sanga <vulcan248@gmail.com>
2023-09-01 12:31:50 +0530

    chore: flex to logo

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -9,8 +9,8 @@ import { UserNav } from "./user-nav";
 export default function Navigation({ session }: { session: Session | null }) {
   return (
     <nav className="flex items-center justify-between w-full px-12 h-16">
-      <div className=" text-md font-semibold flex items-center">
-        <Link href="/">
+      <div className=" text-md font-semibold">
+        <Link className="flex items-center" href="/">
           <ListTodo className="w-8 h-8 mr-2" />
           TODO App
         </Link>
